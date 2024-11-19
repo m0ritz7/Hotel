e1e38d473074dac124729f6173df75c3542c2358
m0ritz7 <moritz.heberle77@gmail.com>
2024-11-19 14:08:52 +0100

    Projektstrukur erstellt

--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,9 @@
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport">
     <link rel="stylesheet" href="style.css" content="width=device-width, initial scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
-<nav class="navbar">
-        <ul class="nav-links">
-            <li><a href="index.php">Startseite</a></li>
-            <li><a href="hilfe.php">Hilfe</a></li>
-            <li><a href="legalnotice.php">Über uns</a></li>
-            <li><a href="login.php">Log in</a></li>
-        </ul>
-</nav>
+<body>
---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+        <ul class="nav-links">
+            <li><a href="index.php">Startseite</a></li>
+            <li><a href="hilfe.php">Hilfe</a></li>
+            <li><a href="legalnotice.php">Über uns</a></li>
+            <li><a href="login.php">Log in</a></li>
+        </ul>
+</nav>
