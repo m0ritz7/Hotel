716222e2a0c0a7b04d2f01fcea1153d1478b96ce
m0ritz7 <moritz.heberle77@gmail.com>
2024-11-19 14:18:14 +0100

    Startseite fix + Ordnerstruktur ergänzt

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar">
         <ul class="nav-links">
-            <li><a href="index.php">Startseite</a></li>
+            <li><a href="home.php">Startseite</a></li>
             <li><a href="hilfe.php">Hilfe</a></li>
             <li><a href="legalnotice.php">Über uns</a></li>
             <li><a href="login.php">Log in</a></li>
